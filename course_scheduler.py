21a7407b28f137a455a749077936d7ac37d9eb5f
breckinridge <BreckStodghill@Breckinridges-MacBook-Pro.local>
2017-10-25 13:47:04 -0500

    Schedules course in Correct order

--- course_scheduler.py
+++ course_scheduler.py
@@ -48,45 +48,128 @@ def schedule(descriptions, goals, init):
     scheduler = Scheduler()
     for goal in goals:
         print(goal)
-        dfs_i(descriptions, goal, init, scheduler)
+        #dfs_i(descriptions, goal, init, scheduler)
+        search(descriptions, goal, init, scheduler)
 
 def find_first_prereq(prereqs, completed):
     # will return first prereq that is unsatisfied
     # if all prereqs are satisfied will return []
-     for prereq in prereqs:
-         for option in prereq:
-             if option not in completed:
-                 return prereq
+     for ors in prereqs:
+         for ands in ors:
+             if ands not in completed:
+                 return ors
      return []
 
+# check to see if the higher level req is a class
+def is_class(prereqs):
+    if all(len(ors) is 1 for ors in prereqs):
+        return True
+    return False
 
-# def search(dictionary, start, discovered, scheduler):
+def test_is_class(dictionary):
+    Course = namedtuple('Course', 'program, designation')
+    CourseInfo = namedtuple('CourseInfo', 'credits, terms, prereqs')
+    courseInfo = dictionary[Course('CS', 'depthothera')]
+    print(courseInfo.prereqs)
+    print(is_class(courseInfo.prereqs))
 
-def dfs_i(dictionary, start, discovered, scheduler):
+def search(dictionary, start, completed, scheduler):
     stack = [start]
     while stack:
         course = stack.pop()
-        print(course)
-        if course not in discovered:
-            # get info
+        if course not in completed:
             courseInfo = dictionary[course]
-            # check in terms
-            discovered.append(course)
-            #if its a course add it to the planner
-            #if courseInfo.credits is not '0':
-            if len(courseInfo.prereqs) is not 0:
-                stack.append(course)
-                tmpStack = []
-                for prereq in find_first_prereq(courseInfo.prereqs):
-                    if prereq not in discovered:
-                        tmpStack.append(prereq)
-                if len(tmpStack) is 0:
-                    scheduler.addCourse(scheduler.currentSemester, course, courseInfo)
+            if courseInfo.credits is '0':
+                # higher level requirement
+                if (is_class(courseInfo.prereqs)):
+                    prereqs = find_first_prereq(courseInfo.prereqs, completed)
+                    if len(prereqs) is not 0:
+                        # this should be size 1 so add prereq to completed and add higher level
+                        for prereq in prereqs:
+                            if prereq not in completed:
+                                print(prereq, '  scheduled')
+                                completed.append(prereq)
+                                print(course, '  scheduled')
+                                completed.append(course)
+
+                    else:
+                        completed.append(course)
+                        print('This course is already satisfied')
                 else:
-                    while tmpStack:
-                        stack.append(tmpStack.pop())
-    for key in scheduler.planner:
-        print(key, scheduler.planner[key])
+                    # if not then look for an or where all ands are satisfied
+                    # if thats not true then look for the most satisfied or and choose it
+                    if is_satisfied(courseInfo.prereqs, completed):
+                        # schedule
+                        completed.append(course)
+                        print(course, '  scheduled')
+                    else:
+                        # add course to the stack
+                        stack.append(course)
+                        # find most satisified or
+                        prereqs = find_most_satisfied_ands(courseInfo.prereqs, completed)
+                        # add each course to stack
+                        tmpstack = []
+                        for prereq in prereqs:
+                            if prereq not in completed:
+                                tmpstack.append(prereq)
+                        while tmpstack:
+                            stack.append(tmpstack.pop())
+            else:
+                # Our course is a course
+                if len(courseInfo.prereqs) is 0:
+                    completed.append(course)
+                    print(course, ' scheduled')
+                else:
+                    # check if one of the ors is satisfied
+                    if is_satisfied(courseInfo.prereqs, completed):
+                        # schedule
+                        completed.append(course)
+                        print(course, ' scheduled')
+                    else:
+                        # add course to the stack
+                        stack.append(course)
+                        # find most satisified or
+                        prereqs = find_most_satisfied_ands(courseInfo.prereqs, completed)
+                        # add each course to stack
+                        tmpstack = []
+                        for prereq in prereqs:
+                            if prereq not in completed:
+                                tmpstack.append(prereq)
+                        while tmpstack:
+                            stack.append(tmpstack.pop())
+
+def test_most_satisfied_mans(dictionary):
+    Course = namedtuple('Course', 'program, designation')
+    CourseInfo = namedtuple('CourseInfo', 'credits, terms, prereqs')
+    courseInfo = dictionary[Course('CS', '3259')]
+    completed = [Course('MATH', '2400')]
+    print(courseInfo.prereqs)
+    print(find_most_satisfied_ands(courseInfo.prereqs, completed))
+
+def find_most_satisfied_ands(prereqs, completed):
+    rv = []
+    max_ands = -1
+    for ors in prereqs:
+        ands = find_ands(ors, completed)
+        if ands > max_ands:
+            rv = ors
+            max_ands = ands
+    return rv
+
+def find_ands(ors, completed):
+    count = 0
+    for ands in ors:
+        if ands in completed:
+            count += 1
+    return count
+
+# Checks to see if a list of prerequisites has a satisfied listing
+def is_satisfied(prereqs, completed):
+    # an or is satisfied id ever and is in completed
+    for ors in prereqs:
+        if all(ands in completed for ands in ors):
+            return True
+    return False
 
 def main(argv):
     test = course_dictionary.create_course_dict()
@@ -95,14 +178,17 @@ def main(argv):
     Course = namedtuple('Course', 'program, designation')
 
     goals = [Course('CS', 'major')]
+    print(test[Course('CS', 'major')])
+    print(test[Course('CS', '3259')])
     # print(test[Course('CS', 'major')])
     init_state = []
     #scheduler_test(test)
-    course_dictionary.print_dict(test)
-    #schedule(test, goals, init_state)
-    test_prereqs(test)
-    print(test[Course('CS', 'calculus')].prereqs)
-
+    #course_dictionary.print_dict(test)
+    schedule(test, goals, init_state)
+    #test_prereqs(test)
+    #test_most_satisfied_mans(test)
+    #print(test[Course('CS', 'calculus')].prereqs)
+    #test_is_class(test)
     print('Done')
 
 if __name__ == "__main__":
